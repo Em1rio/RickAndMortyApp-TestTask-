0428ee3280673d64ff9e76c4459cb04df9dfc36e
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-03-20 14:16:29 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyApp
+//
+//  Created by Emir Nasyrov on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
